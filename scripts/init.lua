5c42fc3f1c53c627ae452ce88fe0b5461e1be7e9
Ladybunne <github@ladybunne.net>
2024-06-11 16:43:02 +1000

    First draft of abilities and items

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,8 @@
 DEBUG = true
 
 Tracker:AddItems("items/abilities.json")
+Tracker:AddItems("items/items.json")
 Tracker:AddMaps("maps/maps.json")
 Tracker:AddLayouts("layouts/abilities.json")
+Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/map.json")
