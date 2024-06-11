b89097d5dff36a3175f02cf1f359d3b89a8ff30c
Ladybunne <github@ladybunne.net>
2024-06-12 07:33:34 +1000

    Added initial locations

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,7 +2,11 @@ DEBUG = true
 
 Tracker:AddItems("items/abilities.json")
 Tracker:AddItems("items/items.json")
+
 Tracker:AddMaps("maps/maps.json")
+
+Tracker:AddLocations("locations/locations.json")
+
 Tracker:AddLayouts("layouts/abilities.json")
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/map.json")
